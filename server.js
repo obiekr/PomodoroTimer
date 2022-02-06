ad1365733df544d71a030feeba62d84b7bda0b00
obiekr <obiekrisnanto1@gmail.com>
2022-02-06 16:21:18 +0700

    redirect to / if /timer has no timer

--- server.js
+++ server.js
@@ -10,13 +10,12 @@ router.get("/", (req, res, next)=>{
     res.render("index", {title: "Express"})
 })
 
-// if (process.env.NODE_ENV === "production"){
-console.log("here")
+if (process.env.NODE_ENV === "production"){
 app.use(express.static("client/build"))
 app.get("*", (req,res) => {
     res.sendFile(path.join(__dirname+"/client/build/index.html"))
 })
-// }
+}
 
 app.listen(port, (err) => {
     if (err) return console.log(err)
