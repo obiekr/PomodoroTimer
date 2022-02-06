50a8059ff10909fb8d31544a294f7a3ec2e0d937
obiekr <obiekrisnanto1@gmail.com>
2022-02-06 16:20:33 +0700

    redirect to / if /timer has no timer

--- client/src/pages/Pomodoro.jsx
+++ client/src/pages/Pomodoro.jsx
@@ -11,6 +11,9 @@ export default function Pomodoro() {
     const [counter, setCounter] = useState(localStorage.getItem("minutes")?parseInt(localStorage.getItem("counter")):0)
     const location = useLocation()
     const data = location.state
+    if(!data){
+      window.location.href = "/"
+    }
     
     if (!localStorage.getItem("minutes")){
         console.log("reset")
@@ -22,45 +25,7 @@ export default function Pomodoro() {
     }
     const [minutes, setMinutes] = useState(parseInt(localStorage.getItem("minutes")))
     const [seconds, setSeconds] = useState(parseInt(localStorage.getItem("seconds")))
-
-    // let a = false
-    // let ts1 = 0
-    // document.body.addEventListener("keydown", (e)=>{
-    //   let ts2 = new Date()/1000
-    //   if (ts2-ts1 >= 2 & e.code === "Space" & !a){
-    //     console.log(ts2-ts1 >= 1)
-    //     ts1=ts2
-    //     a=true
-    //   }
-    //   document.body.addEventListener('keyup', ()=>{
-    //     a=false
-    //   })
-    // })
-
-    // let ts1 = new Date()/1000
-    // document.body.addEventListener("keydown", ()=>{
-    //   let ts2 = new Date()/1000
-    //   if (ts2-ts1 >= 2){
-    //   console.log(ts2-ts1, ts2-ts1 >= 10)
-    //     ts1=ts2
-    //   }
-    // })
-
-
-    //   let a = false
-    // document.body.addEventListener('keydown', (e)=>{
-    //   if (e.code === "Space" & !a){
-    //     // setCounting(!counting)
-    //     console.log("pressed")
-    //     a = true
-
-    //   }
-    //   document.body.addEventListener('keyup', ()=>{
-    //     a=false
-    //   })
-    // })
     
-
     return (
         <div className='container-fluid p-0 m-0'>
           <div className="d-flex justify-content-center vh-100">
